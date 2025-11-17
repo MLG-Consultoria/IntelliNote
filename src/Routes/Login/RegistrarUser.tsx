3eed6e446f3b5995a2c6c1ea88891ca6c4e42128
RuralGiovane <giovaneamato@gmail.com>
2025-11-17 18:28:50 -0300

    Feat: Adicionando conteudo substitutivo

---
+++ src/Routes/Login/RegistrarUser.tsx
@@ -0,0 +1,10 @@
+const RegistrarUser = () => {
+  return (
+    <div>
+        <h1>Registrar Usuário</h1>
+        {/* Formulário de registro de usuário aqui */}
+    </div>
+  );
+}
+
+export default RegistrarUser;
