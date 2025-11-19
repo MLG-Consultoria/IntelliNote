6ef6a9e5e84f03f78889759bae843cccd3739d50
roque-arantes <mrarantes07@gmail.com>
2025-11-19 11:15:39 -0300

    Feat: Adicionando todas as rotas do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 
-import DarkModeToggle from './Components/DarkModeToggle'
 import Header from './Components/Header'
-import MenuPrincipal from './Routes/MenuPrincipal';
-import PagLogin from './Routes/PagLogin';
-import Integrantes from './Routes/Intergrantes';
-import SobreNos from './Routes/SobreNos';
-import Faq from './Routes/Faq';
-import VerNota from './Routes/VerNota';
-import CriarNota from './Routes/CriarNota';
+import MenuPrincipal from './Routes/Outros/MenuPrincipal';
+import PagLogin from './Routes/Login/PagLogin';
+import RegistrarUser from './Routes/Login/RegistrarUser';
+import Integrantes from './Routes/Outros/Integrantes/Intergrantes';
+import IntegrantesSolo from './Routes/Outros/Integrantes/IntegrantesSolo';
+import SobreNos from './Routes/Outros/SobreNos';
+import Faq from './Routes/Outros/Faq';
+import Contato from './Routes/Outros/Contato';
+import VerNota from './Routes/MainFunctions/VerNota';
+import CriarNota from './Routes/MainFunctions/CriarNota';
+import Calendario from './Routes/MainFunctions/Calendario';
+import ReSkilling from './Routes/MainFunctions/ReSkilling';
+import UpSkilling from './Routes/MainFunctions/UpSkilling';
 
 import Footer from './Components/Footer'
 
@@ -17,20 +22,24 @@ import Footer from './Components/Footer'
 function App() {
   return (
     <>
-      <main className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <main className="flex flex-col min-h-screen bg-gray-100 dark:bg-linear-to-br from-gray-900 to-gray-950 text-gray-900 dark:text-gray-100">
         <Header />
-        <DarkModeToggle /> 
 
         <div>
           <Routes>
             <Route path="/" element={<PagLogin />} />
+            <Route path="/Registro" element={<RegistrarUser />} />
             <Route path="/MenuPrincipal" element={<MenuPrincipal />} />
             <Route path="/Integrantes" element={<Integrantes />} />
-              <Route path="/Faq" element={<Faq />} />
+            <Route path="/Integrantes/:nome" element={<IntegrantesSolo />} />
+            <Route path="/Faq" element={<Faq />} />
             <Route path="/SobreNos" element={<SobreNos />} />
+            <Route path="/Contato" element={<Contato />} />
             <Route path="/VerNota" element={<VerNota />} />
             <Route path="/CriarNota" element={<CriarNota />} />
-
+            <Route path="/Calendario" element={<Calendario />} />
+            <Route path="/ReSkilling" element={<ReSkilling />} />
+            <Route path="/UpSkilling" element={<UpSkilling />} />
           </Routes>
         </div>
         <Footer />
